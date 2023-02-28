1356a4cae90cd6e26dd783167bb7b519b2f0a058
AshleyWrightA <AshleyWrightAppDev@outlook.com>
2023-02-28 13:26:21 -0700

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/meyer-reset.css";
+import "./css/index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<React.StrictMode></React.StrictMode>);
